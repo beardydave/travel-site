743e4d37f1c77f9973e29f9d2bce10ae84c47857
David <davidwhittaker89@gmail.com>
2020-07-09 18:37:07 +0100

    finished styling large-hero and buttons

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import "../styles/main.scss";
 
-
+if (module.hot) {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 const path = require("path");
 
-
 module.exports = {
   entry: "./app/assets/scripts/App.js",
   output: {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app"),
   },
+  devServer: {
+    before: function (app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
   mode: "development",
-  watch: true,
-
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
